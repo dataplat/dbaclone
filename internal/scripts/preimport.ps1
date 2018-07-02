27183bdc8b6da0afa20b239dedb5c03d413a3d1a
Sander Stad <sander@sqlstad.nl>
2018-07-02 11:33:12 +0200

    This fixes #10

--- internal/scripts/preimport.ps1
+++ internal/scripts/preimport.ps1
@@ -1,4 +1,3 @@
-# Add all things you want to run before importing the main code
 # Check if window is in elevated mode
 $elevated = ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
 if ( -not $elevated ) {
