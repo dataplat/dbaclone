c42a244119e7e5d11431c2c10c19f1c3b9eedd8f
Sander Stad <sander@sqlstad.nl>
2018-07-04 20:31:03 +0200

    Fixed name of module

--- internal/tepp/assignment.ps1
+++ internal/tepp/assignment.ps1
@@ -1,4 +1,4 @@
-<#
+﻿<#
 # Example:
-Register-PSFTeppArgumentCompleter -Command Get-Alcohol -Parameter Type -Name PSDatabasClone.alcohol
+Register-PSFTeppArgumentCompleter -Command Get-Alcohol -Parameter Type -Name PSDatabaseClone.alcohol
 #>
