aa2c7534936d8e6e16d9624b8364651fa90e2c9d
Sander Stad <sander@sqlstad.nl>
2018-07-05 10:59:17 +0200

    Fixed test

--- tests/functions/New-PDCVhdDisk.Tests.ps1
+++ tests/functions/New-PDCVhdDisk.Tests.ps1
@@ -1,7 +1,6 @@
-﻿$commandname = $MyInvocation.MyCommand.Name.Replace(".Tests.ps1", "")
+﻿$moduleRoot = (Resolve-Path "$PSScriptRoot\..\..").Path
+$commandname = $MyInvocation.MyCommand.Name.Replace(".Tests.ps1", "")
 $rootPath = Split-Path (Split-Path $PSScriptRoot -Parent) -Parent
-
-Write-Host -Object "Running $PSCommandpath" -ForegroundColor Cyan
 
 . "$rootPath\tests\constants.ps1"
 
@@ -11,27 +10,26 @@ Describe "$commandname Unit Tests" {
         # Get a random value for the database name
         $random = Get-Random
 
-        $filename = "Test_VHD_$($random).vhdx"
         $name = "Test_VHD_$($random)"
-        $destination = "$rootPath\tests"
-        $path = "$destination\$filename"
+        $destination = "$moduleRoot\tests"
+        $path = "$destination\$name.vhdx"
     }
 
-    Context "Create VHD with file name" {
-        It -Skip "Should be successful" {
-            $null = New-PDCVhdDisk -Destination $destination -FileName "$name.vhdx"
+    Context "Create VHD with -FileName parameter" {
+        $null = New-PDCVhdDisk -Destination $destination -FileName "$name.vhdx"
 
-            (Test-Path -Path $path) | Should -Be $true
+        It "Should be true" {
+            (Test-Path -Path $path) | Should Be $true
         }
 
         Remove-Item -Path $path -Force
     }
 
-    Context "Create VHD with name" {
-        It -Skip "Should be successful" {
-            $null = New-PDCVhdDisk -Destination $destination -Name $name
+    Context "Create VHD with -Name parameter" {
+        $null = New-PDCVhdDisk -Destination $destination -Name $name
 
-            (Test-Path -Path $path) | Should -Be $true
+        It "Should be true" {
+            (Test-Path -Path $path) | Should Be $true
 
         }
 
