c1a2388c3a1159ae5d56625b8a27c8b2545d7ae8
Sander Stad <sander@sqlstad.nl>
2018-07-11 00:20:40 +0200

    Fixed remote execution. Fixes #19

--- functions/Remove-PSDCImage.ps1
+++ functions/Remove-PSDCImage.ps1
@@ -97,7 +97,7 @@
             $pdcCredential = Get-PSFConfigValue -FullName psdatabaseclone.database.credential -Fallback $null
         }
         else {
-            $pdcCredential = $pdcCredential
+            $pdcCredential = $PSDCSqlCredential
         }
 
         # Test the module database setup
@@ -111,7 +111,7 @@
         Write-PSFMessage -Message "Started removing database images" -Level Verbose
 
         # Get all the items
-        $items = Get-PSDCImage -pdcCredential $pdcCredential
+        $items = Get-PSDCImage -PSDCSqlCredential $pdcCredential
 
         if ($ImageID) {
             Write-PSFMessage -Message "Filtering image ids" -Level Verbose
@@ -154,23 +154,36 @@
             # Loop through each of the results
             foreach ($item in $image.Group) {
 
+                # Make up the data from the network path
+                try {
+                    [uri]$uri = New-Object System.Uri($item.ImageLocation)
+                    $uriHost = $uri.Host
+                }
+                catch {
+                    Stop-PSFFunction -Message "The image location $ImageNetworkPath is not valid" -ErrorRecord $_ -Target $ImageNetworkPath
+                    return
+                }
+
+                # Setup the computer object
+                $computer = [PsfComputer]$uriHost
+
                 $query = "
-                SELECT c.CloneLocation,
-                        c.AccessPath,
-                        c.SqlInstance,
-                        c.DatabaseName,
-                        h.HostName,
-                        h.IPAddress,
-                        h.FQDN,
-                        i.ImageLocation
-                FROM dbo.Image as i
-                    INNER JOIN dbo.Clone AS c
-                        ON c.ImageID = i.ImageID
-                    INNER JOIN dbo.Host AS h
-                        ON h.HostID = c.HostID
-                WHERE i.ImageID = $($item.ImageID)
-                ORDER BY h.HostName;
-            "
+                    SELECT c.CloneLocation,
+                            c.AccessPath,
+                            c.SqlInstance,
+                            c.DatabaseName,
+                            h.HostName,
+                            h.IPAddress,
+                            h.FQDN,
+                            i.ImageLocation
+                    FROM dbo.Image as i
+                        INNER JOIN dbo.Clone AS c
+                            ON c.ImageID = i.ImageID
+                        INNER JOIN dbo.Host AS h
+                            ON h.HostID = c.HostID
+                    WHERE i.ImageID = $($item.ImageID)
+                    ORDER BY h.HostName;
+                "
 
                 # Try to get the neccesary info from the EasyClone database
                 try {
@@ -187,7 +200,7 @@
                             # Remove the clones for the host
                             try {
                                 Write-PSFMessage -Message "Removing clones for host $($result.HostName) and database $($result.DatabaseName)" -Level Verbose
-                                Remove-PSDCClone -HostName $result.HostName -Database $result.DatabaseName -pdcCredential $pdcCredential -Credential $Credential -Confirm:$false
+                                Remove-PSDCClone -HostName $result.HostName -Database $result.DatabaseName -PSDCSqlCredential $pdcCredential -Credential $Credential -Confirm:$false
                             }
                             catch {
                                 Stop-PSFFunction -Message "Couldn't remove clones from host $($result.HostName)" -ErrorRecord $_ -Target $result -Continue
@@ -204,12 +217,27 @@
 
                 # Remove the image from the file system
                 try {
-                    if (Test-Path -Path $item.ImageLocation -Credential $Credential) {
-                        Write-PSFMessage -Message "Removing image '$($item.ImageLocation)' from file system" -Level Verbose
-                        $null = Remove-Item -Path $item.ImageLocation -Credential $Credential -Force:$Force
+                    if ($computer.IsLocalhost) {
+                        if (Test-Path -Path $item.ImageLocation -Credential $Credential) {
+                            Write-PSFMessage -Message "Removing image '$($item.ImageLocation)' from file system" -Level Verbose
+
+                            $null = Remove-Item -Path $item.ImageLocation -Credential $Credential -Force:$Force
+                        }
+                        else {
+                            Write-PSFMessage -Message "Couldn't find image $($item.ImageLocation)" -Level Verbose
+                        }
                     }
                     else {
-                        Write-PSFMessage -Message "Couldn't find image $($item.ImageLocation)" -Level Verbose
+                        $command = [scriptblock]::Create("Test-Path -Path $($item.ImageLocation)")
+                        $result = Invoke-PSFCommand -ComputerName $computer -ScriptBlock $command -Credential $Credential
+
+                        if ($result) {
+                            $command = [scriptblock]::Create("Remove-Item -Path $($item.ImageLocation) -Force")
+                            $result = Invoke-PSFCommand -ComputerName $computer -ScriptBlock $command -Credential $Credential
+                        }
+                        else {
+                            Write-PSFMessage -Message "Couldn't find image $($item.ImageLocation)" -Level Verbose
+                        }
                     }
                 }
                 catch {
