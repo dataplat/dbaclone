ab84691632a379d05a6f962c3da6e6a7eba2d489
Sander Stad <sander@sqlstad.nl>
2018-07-25 18:06:33 +0200

    Changed output messages back to write-host

--- tests/appveyor/preparation.ps1
+++ tests/appveyor/preparation.ps1
@@ -6,28 +6,23 @@ Write-PSFMessage -Message "appveyor.prep: Install PSScriptAnalyzer" -Level Host
 Install-Module -Name PSScriptAnalyzer -Force -SkipPublisherCheck | Out-Null
 
 # Get Pester (to run tests)
-#Write-Host -Object "appveyor.prep: Install Pester" -ForegroundColor DarkGreen
-Write-PSFMessage -Message "appveyor.prep: Install Pester" -Level Host
+Write-Host -Object "appveyor.prep: Install Pester" -ForegroundColor DarkGreen
 choco install pester | Out-Null
 
 # Get dbatools
-#Write-Host -Object "appveyor.prep: Install dbatools" -ForegroundColor DarkGreen
-Write-PSFMessage -Message "appveyor.prep: Install dbatools" -Level Host
+Write-Host -Object "appveyor.prep: Install dbatools" -ForegroundColor DarkGreen
 Install-Module -Name dbatools | Out-Null
 
 # Get PSFramework
-#Write-Host -Object "appveyor.prep: Install PSFramework" -ForegroundColor DarkGreen
-Write-PSFMessage -Message "appveyor.prep: Install PSFramework" -Level Host
+Write-Host -Object "appveyor.prep: Install PSFramework" -ForegroundColor DarkGreen
 Install-Module -Name PSFramework | Out-Null
 
 # Get Hyper-V-PowerShell
-#Write-Host -Object "appveyor.prep: Install Hyper-V-PowerShell" -ForegroundColor DarkGreen
-Write-PSFMessage -Message "appveyor.prep: Install Hyper-V-PowerShell" -Level Host
+Write-Host -Object "appveyor.prep: Install Hyper-V-PowerShell" -ForegroundColor DarkGreen
 Install-WindowsFeature -Name Hyper-V-PowerShell | Out-Null
 
 # Creating config files
-#Write-Host -Object "appveyor.prep: Creating configurations files" -ForegroundColor DarkGreen
-Write-PSFMessage -Message "appveyor.prep: Creating configurations files" -Level Host
+Write-Host -Object "appveyor.prep: Creating configurations files" -ForegroundColor DarkGreen
 $configPath = "c:\projects\config"
 
 $null = New-Item -Path "$configPath\hosts.json" -Force:$Force
@@ -35,15 +30,13 @@ $null = New-Item -Path "$configPath\images.json" -Force:$Force
 $null = New-Item -Path "$configPath\clones.json" -Force:$Force
 
 # Setting configurations
-#Write-Host -Object "appveyor.prep: Setting configurations" -ForegroundColor DarkGreen
-Write-PSFMessage -Message "appveyor.prep: Setting module configurations" -Level Host
+Write-Host -Object "appveyor.prep: Setting configurations" -ForegroundColor DarkGreen
 Set-PSFConfig -Module PSDatabaseClone -Name setup.status -Value $true -Validation bool
 Set-PSFConfig -Module PSDatabaseClone -Name informationstore.mode -Value 'File'
 Set-PSFConfig -Module PSDatabaseClone -Name informationstore.path -Value "$configPath" -Validation string
 
 # Registering configurations
-#Write-Host -Object "appveyor.prep: Registering configurations" -ForegroundColor DarkGreen
-Write-PSFMessage -Message "appveyor.prep: Registering module configurations" -Level Host
+Write-Host -Object "appveyor.prep: Registering configurations" -ForegroundColor DarkGreen
 Get-PSFConfig -FullName psdatabaseclone.setup.status | Register-PSFConfig -Scope SystemDefault
 Get-PSFConfig -FullName psdatabaseclone.informationstore.mode | Register-PSFConfig -Scope SystemDefault
 Get-PSFConfig -FullName psdatabaseclone.informationstore.path | Register-PSFConfig -Scope SystemDefault
