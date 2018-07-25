e45994a364e99e3451791a56103b8f55e11d536f
Sander Stad <sander@sqlstad.nl>
2018-07-25 15:26:04 +0200

    Added appveyor functionality

---
+++ tests/appveyor_preparation.ps1
@@ -0,0 +1,18 @@
+Add-AppveyorTest -Name "appveyor.prep" -Framework NUnit -FileName "appveyor.prep.ps1" -Outcome Running
+$sw = [system.diagnostics.stopwatch]::startNew()
+
+#Get PSScriptAnalyzer (to check warnings)
+Write-Host -Object "appveyor.prep: Install PSScriptAnalyzer" -ForegroundColor DarkGreen
+Install-Module -Name PSScriptAnalyzer -Force -SkipPublisherCheck | Out-Null
+
+#Get Pester (to run tests)
+Write-Host -Object "appveyor.prep: Install Pester" -ForegroundColor DarkGreen
+choco install pester | Out-Null
+
+#Get PSFramework
+Write-Host -Object "appveyor.prep: Install PSFramework" -ForegroundColor DarkGreen
+Install-Module -Name PSFramework | Out-Null
+
+
+$sw.Stop()
+Update-AppveyorTest -Name "appveyor.prep" -Framework NUnit -FileName "appveyor.prep.ps1" -Outcome Passed -Duration $sw.ElapsedMilliseconds
