1e10f87b004999379f7ba70cc8de014f154eada6
Sander Stad <sander@sqlstad.nl>
2018-09-10 10:08:06 +0200

    Changed path to database script

--- functions/support/Set-PSDCConfiguration.ps1
+++ functions/support/Set-PSDCConfiguration.ps1
@@ -283,7 +283,7 @@
             # Setup the path to the sql file
             if ($newDatabase) {
                 try {
-                    $path = "$($MyInvocation.MyCommand.Module.ModuleBase)\internal\resources\database.sql"
+                    $path = "$($MyInvocation.MyCommand.Module.ModuleBase)\internal\resources\database\database.sql"
                     $query = [System.IO.File]::ReadAllText($path)
 
                     # Create the objects
