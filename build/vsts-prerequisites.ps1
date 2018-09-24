7a21bcd49498a151728f431fb0cab52cbeeeee72
Sander Stad <sander@sqlstad.nl>
2018-09-24 17:19:01 +0200

    Added dbatools install

--- build/vsts-prerequisites.ps1
+++ build/vsts-prerequisites.ps1
@@ -2,3 +2,5 @@ Write-Host "Installing Pester" -ForegroundColor Cyan
 Install-Module Pester -Force -SkipPublisherCheck
 Write-Host "Installing PSFramework" -ForegroundColor Cyan
 Install-Module PSFramework -Force -SkipPublisherCheck
+Write-Host "Installing dbatools" -ForegroundColor Cyan
+Install-Module dbatools -Force -SkipPublisherCheck
