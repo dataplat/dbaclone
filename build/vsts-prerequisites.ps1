8a526cfdcc31bc092f1a1bcd215f109b8f529868
Sander Stad <sander@sqlstad.nl>
2018-09-24 18:19:22 +0200

    Removed relative path

--- build/vsts-prerequisites.ps1
+++ build/vsts-prerequisites.ps1
@@ -5,7 +5,7 @@ Install-Module PSFramework -Force -SkipPublisherCheck
 Write-Host "Installing dbatools" -ForegroundColor Cyan
 Install-Module dbatools -Force -SkipPublisherCheck
 
-. "$PSScriptRoot\..\..\..\build\vsts-constants.ps1"
+. "$PSScriptRoot\vsts-constants.ps1"
 
 # Creating folder
 Write-Host -Object "Creating image and clone directories" -ForegroundColor Cyan
