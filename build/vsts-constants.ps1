5289a8162c93b22b8b8e33d24a6d86ce7ad57f6d
Sander Stad <sander@sqlstad.nl>
2018-09-24 23:10:43 +0200

    Changed path in constants

--- build/vsts-constants.ps1
+++ build/vsts-constants.ps1
@@ -1,6 +1,6 @@
 ﻿# constants
 $publishDir = New-Item -Path $env:SYSTEM_DEFAULTWORKINGDIRECTORY -Name publish -ItemType Directory
-$rootPath = "$($publishDir.FullName)\clones"
+$rootPath = "$($publishDir.FullName)\psdc"
 $workingfolder = "$rootPath"
 $jsonfolder = "$rootPath\config"
 $imagefolder = "$rootPath\images"
