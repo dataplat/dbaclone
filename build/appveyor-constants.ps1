ed8e8b67ff91b02ca31fa092e06fab93f06d552a
Sander Stad <sander@sqlstad.nl>
2018-10-24 00:25:26 +0200

    Changed working folder

--- build/appveyor-constants.ps1
+++ build/appveyor-constants.ps1
@@ -1,7 +1,7 @@
 ﻿# constants
 #$publishDir = "C:\Projects"
 #$workingfolder = "$($env:APPDATA)\psdc"
-$workingfolder = "c:\projects\psdc"
+$workingfolder = "C:\projects\psdatabaseclone"
 $jsonfolder = "$workingfolder\config"
 $imagefolder = "$workingfolder\images"
 $clonefolder = "$workingfolder\clones"
