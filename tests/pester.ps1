8522a794abc17e4493ad10de04f2de5dde426b4e
sqlstad <sanderstad@gmail.com>
2019-10-21 23:06:11 +0200

    Added run of command for configuration

--- tests/pester.ps1
+++ tests/pester.ps1
@@ -1,13 +1,13 @@
 param (
 	$TestGeneral = $true,
-	
+
 	$TestFunctions = $true,
-	
+
 	[ValidateSet('None', 'Default', 'Passed', 'Failed', 'Pending', 'Skipped', 'Inconclusive', 'Describe', 'Context', 'Summary', 'Header', 'Fails', 'All')]
 	$Show = "None",
-	
+
 	$Include = "*",
-	
+
 	$Exclude = ""
 )
 
@@ -19,6 +19,8 @@ Remove-Module PSDatabaseClone -ErrorAction Ignore
 Import-Module "$PSScriptRoot\..\PSDatabaseClone.psd1"
 Import-Module "$PSScriptRoot\..\PSDatabaseClone.psm1" -Force
 
+$null = Set-PSDCConfiguration -InformationStore File -Path $script:workingfolder -Force -EnableException
+
 Write-PSFMessage -Level Important -Message "Creating test result folder"
 $null = New-Item -Path "$PSScriptRoot\..\.." -Name TestResults -ItemType Directory -Force
 
@@ -29,23 +31,21 @@ $testresults = @()
 
 #region Run General Tests
 Write-PSFMessage -Level Important -Message "Modules imported, proceeding with general tests"
-foreach ($file in (Get-ChildItem "$PSScriptRoot\general" -Filter "*.Tests.ps1"))
-{
+foreach ($file in (Get-ChildItem "$PSScriptRoot\general" -Filter "*.Tests.ps1")) {
 	Write-PSFMessage -Level Significant -Message "  Executing <c='em'>$($file.Name)</c>"
 	$TestOuputFile = Join-Path "$PSScriptRoot\..\..\TestResults" "TEST-$($file.BaseName).xml"
-    $results = Invoke-Pester -Script $file.FullName -Show $Show -PassThru -OutputFile $TestOuputFile -OutputFormat NUnitXml
-	foreach ($result in $results)
-	{
+	$results = Invoke-Pester -Script $file.FullName -Show $Show -PassThru -OutputFile $TestOuputFile -OutputFormat NUnitXml
+	foreach ($result in $results) {
 		$totalRun += $result.TotalCount
 		$totalFailed += $result.FailedCount
 		$result.TestResult | Where-Object { -not $_.Passed } | ForEach-Object {
 			$name = $_.Name
 			$testresults += [pscustomobject]@{
-				Describe  = $_.Describe
-				Context   = $_.Context
-				Name	  = "It $name"
-				Result    = $_.Result
-				Message   = $_.FailureMessage
+				Describe = $_.Describe
+				Context  = $_.Context
+				Name     = "It $name"
+				Result   = $_.Result
+				Message  = $_.FailureMessage
 			}
 		}
 	}
@@ -54,26 +54,24 @@ foreach ($file in (Get-ChildItem "$PSScriptRoot\general" -Filter "*.Tests.ps1"))
 
 #region Test Commands
 Write-PSFMessage -Level Important -Message "Proceeding with individual tests"
-foreach ($file in (Get-ChildItem "$PSScriptRoot\functions" -Recurse -File -Filter "*Tests.ps1"))
-{
+foreach ($file in (Get-ChildItem "$PSScriptRoot\functions" -Recurse -File -Filter "*Tests.ps1")) {
 	if ($file.Name -notlike $Include) { continue }
 	if ($file.Name -like $Exclude) { continue }
-	
+
 	Write-PSFMessage -Level Significant -Message "  Executing $($file.Name)"
 	$TestOuputFile = Join-Path "$PSScriptRoot\..\..\TestResults" "TEST-$($file.BaseName).xml"
-    $results = Invoke-Pester -Script $file.FullName -Show $Show -PassThru -OutputFile $TestOuputFile -OutputFormat NUnitXml
-	foreach ($result in $results)
-	{
+	$results = Invoke-Pester -Script $file.FullName -Show $Show -PassThru -OutputFile $TestOuputFile -OutputFormat NUnitXml
+	foreach ($result in $results) {
 		$totalRun += $result.TotalCount
 		$totalFailed += $result.FailedCount
 		$result.TestResult | Where-Object { -not $_.Passed } | ForEach-Object {
 			$name = $_.Name
 			$testresults += [pscustomobject]@{
-				Describe   = $_.Describe
-				Context    = $_.Context
-				Name	   = "It $name"
-				Result	   = $_.Result
-				Message    = $_.FailureMessage
+				Describe = $_.Describe
+				Context  = $_.Context
+				Name     = "It $name"
+				Result   = $_.Result
+				Message  = $_.FailureMessage
 			}
 		}
 	}
@@ -85,7 +83,6 @@ $testresults | Sort-Object Describe, Context, Name, Result, Message | Format-Lis
 if ($totalFailed -eq 0) { Write-PSFMessage -Level Critical -Message "All <c='em'>$totalRun</c> tests executed without a single failure!" }
 else { Write-PSFMessage -Level Critical -Message "<c='em'>$totalFailed tests</c> out of <c='sub'>$totalRun</c> tests failed!" }
 
-if ($totalFailed -gt 0)
-{
+if ($totalFailed -gt 0) {
 	throw "$totalFailed / $totalRun tests failed!"
 }
