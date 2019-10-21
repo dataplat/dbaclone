3b12604e580623d5aadf88d811343d1902df60ce
sqlstad <sanderstad@gmail.com>
2019-10-21 18:46:01 +0200

    Added integration tests

--- PSDatabaseClone/tests/functions/support/Convert-PSDCUncPathToLocalPath.Tests.ps1
+++ PSDatabaseClone/tests/functions/support/Convert-PSDCUncPathToLocalPath.Tests.ps1
@@ -11,3 +11,37 @@ Describe "$CommandName Unit Tests" -Tag 'UnitTests' {
         }
     }
 }
+
+Describe "$CommandName Integration Tests" -Tag "IntegrationTests" {
+
+    BeforeAll {
+        if (-not (Test-Path -Path $script:workingfolder)) {
+            New-Item -Path $script:workingfolder -ItemType Directory
+        }
+
+        if (-not (Get-SmbShare -Name $script:psdcshare -ErrorAction SilentlyContinue)) {
+            New-SMBShare -Name $script:psdcshare -Path $script:workingfolder -FullAccess Everyone
+        }
+
+        $shares = Get-SmbShare
+    }
+
+    Context "Convert UNC to local path" {
+        It "Share should exist" {
+            $shares.Name | Should -Contain $script:psdcshare
+        }
+
+        $result = Convert-PSDCUncPathToLocalPath -UncPath "\\127.0.0.1\$($script:psdcshare)"
+
+        It "Result should be the same as local path" {
+            $result | Should -Be $script:workingfolder
+        }
+    }
+
+    AfterAll {
+        if (Test-Path -Path $script:workingfolder) {
+            Remove-Item -Path $script:workingfolder -Confirm:$false -Force -Recurse
+        }
+    }
+
+}
