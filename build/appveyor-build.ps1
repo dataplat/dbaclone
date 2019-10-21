17f8e9fd9c2d64a6a2116329f1e7ec19a6d6c365
sqlstad <sanderstad@gmail.com>
2019-10-21 23:18:00 +0200

    Fixed paths

--- build/appveyor-build.ps1
+++ build/appveyor-build.ps1
@@ -39,7 +39,7 @@ if (-not $WorkingDirectory) {
 try {
     Write-PSFMessage -Level Important -Message "Creating and populating publishing directory"
     $publishDir = New-Item -Path $WorkingDirectory -Name publish -ItemType Directory
-    Copy-Item -Path "$($WorkingDirectory)\PStSQLtTestGenerator" -Destination $publishDir.FullName -Recurse -Force
+    Copy-Item -Path "$($WorkingDirectory)\PSDatabaseClone" -Destination $publishDir.FullName -Recurse -Force
 }
 catch {
     Stop-PSFFunction -Message "Something went wrong creating and populating publishing directory" -Target $publishDir -ErrorRecord $_
@@ -48,10 +48,10 @@ catch {
 
 # region remove unneccesary directories
 try {
-    Remove-Item -Path "$($publishDir.FullName)\PStSQLtTestGenerator\appveyor.yml" -Force -Recurse
-    Remove-Item -Path "$($publishDir.FullName)\PStSQLtTestGenerator\build" -Force -Recurse
-    Remove-Item -Path "$($publishDir.FullName)\PStSQLtTestGenerator\resources" -Force -Recurse
-    Remove-Item -Path "$($publishDir.FullName)\PStSQLtTestGenerator\tests" -Force -Recurse
+    Remove-Item -Path "$($publishDir.FullName)\PSDatabaseClone\appveyor.yml" -Force -Recurse
+    Remove-Item -Path "$($publishDir.FullName)\PSDatabaseClone\build" -Force -Recurse
+    Remove-Item -Path "$($publishDir.FullName)\PSDatabaseClone\resources" -Force -Recurse
+    Remove-Item -Path "$($publishDir.FullName)\PSDatabaseClone\tests" -Force -Recurse
 }
 catch {
     Stop-PSFFunction -Message "Could not remove directories" -Target $publishDir.FullName -ErrorRecord $_
@@ -67,7 +67,7 @@ $processed = @()
 foreach ($line in (Get-Content "$($PSScriptRoot)\filesBefore.txt" | Where-Object { $_ -notlike "#*" })) {
     if ([string]::IsNullOrWhiteSpace($line)) { continue }
 
-    $basePath = Join-Path -Path "$($publishDir.FullName)\PStSQLtTestGenerator" -ChildPath $line
+    $basePath = Join-Path -Path "$($publishDir.FullName)\PSDatabaseClone" -ChildPath $line
     foreach ($entry in (Resolve-PSFPath -Path $basePath)) {
         $item = Get-Item $entry
         if ($item.PSIsContainer) { continue }
@@ -78,10 +78,10 @@ foreach ($line in (Get-Content "$($PSScriptRoot)\filesBefore.txt" | Where-Object
 }
 
 # Gather commands
-Get-ChildItem -Path "$($publishDir.FullName)\PStSQLtTestGenerator\internal\functions\" -Recurse -File -Filter "*.ps1" | ForEach-Object {
+Get-ChildItem -Path "$($publishDir.FullName)\PSDatabaseClone\internal\functions\" -Recurse -File -Filter "*.ps1" | ForEach-Object {
     $text += [System.IO.File]::ReadAllText($_.FullName)
 }
-Get-ChildItem -Path "$($publishDir.FullName)\PStSQLtTestGenerator\functions\" -Recurse -File -Filter "*.ps1" | ForEach-Object {
+Get-ChildItem -Path "$($publishDir.FullName)\PSDatabaseClone\functions\" -Recurse -File -Filter "*.ps1" | ForEach-Object {
     $text += [System.IO.File]::ReadAllText($_.FullName)
 }
 
@@ -89,7 +89,7 @@ Get-ChildItem -Path "$($publishDir.FullName)\PStSQLtTestGenerator\functions\" -R
 foreach ($line in (Get-Content "$($PSScriptRoot)\filesAfter.txt" | Where-Object { $_ -notlike "#*" })) {
     if ([string]::IsNullOrWhiteSpace($line)) { continue }
 
-    $basePath = Join-Path "$($publishDir.FullName)\PStSQLtTestGenerator" $line
+    $basePath = Join-Path "$($publishDir.FullName)\PSDatabaseClone" $line
     foreach ($entry in (Resolve-PSFPath -Path $basePath)) {
         $item = Get-Item $entry
         if ($item.PSIsContainer) { continue }
@@ -101,10 +101,10 @@ foreach ($line in (Get-Content "$($PSScriptRoot)\filesAfter.txt" | Where-Object 
 #endregion Gather text data to compile
 
 #region Update the psm1 file
-$fileData = Get-Content -Path "$($publishDir.FullName)\PStSQLtTestGenerator\PStSQLtTestGenerator.psm1" -Raw
+$fileData = Get-Content -Path "$($publishDir.FullName)\PSDatabaseClone\PSDatabaseClone.psm1" -Raw
 $fileData = $fileData.Replace('"<was not compiled>"', '"<was compiled>"')
 $fileData = $fileData.Replace('"<compile code into here>"', ($text -join "`n`n"))
-[System.IO.File]::WriteAllText("$($publishDir.FullName)\PStSQLtTestGenerator\PStSQLtTestGenerator.psm1", $fileData, [System.Text.Encoding]::UTF8)
+[System.IO.File]::WriteAllText("$($publishDir.FullName)\PSDatabaseClone\PSDatabaseClone.psm1", $fileData, [System.Text.Encoding]::UTF8)
 #endregion Update the psm1 file
 
 #region Updating the Module Version
@@ -113,16 +113,16 @@ Write-PSFMessage -Level Important -Message "Branch: $($env:APPVEYOR_REPO_BRANCH)
 if ($SkipPublish) { return }
 if ($AutoVersion) {
     Write-PSFMessage -Level Important -Message "Updating module version numbers."
-    try { [version]$remoteVersion = (Find-Module 'PStSQLtTestGenerator' -Repository $Repository -ErrorAction Stop).Version }
+    try { [version]$remoteVersion = (Find-Module 'PSDatabaseClone' -Repository $Repository -ErrorAction Stop).Version }
     catch {
         Stop-PSFFunction -Message "Failed to access $($Repository)" -EnableException $true -ErrorRecord $_
     }
     if (-not $remoteVersion) {
-        Stop-PSFFunction -Message "Couldn't find PStSQLtTestGenerator on repository $($Repository)" -EnableException $true
+        Stop-PSFFunction -Message "Couldn't find PSDatabaseClone on repository $($Repository)" -EnableException $true
     }
     $newBuildNumber = $remoteVersion.Build + 1
-    [version]$localVersion = (Import-PowerShellDataFile -Path "$($publishDir.FullName)\PStSQLtTestGenerator\PStSQLtTestGenerator.psd1").ModuleVersion
-    Update-ModuleManifest -Path "$($publishDir.FullName)\PStSQLtTestGenerator\PStSQLtTestGenerator.psd1" -ModuleVersion "$($localVersion.Major).$($localVersion.Minor).$($newBuildNumber)"
+    [version]$localVersion = (Import-PowerShellDataFile -Path "$($publishDir.FullName)\PSDatabaseClone\PSDatabaseClone.psd1").ModuleVersion
+    Update-ModuleManifest -Path "$($publishDir.FullName)\PSDatabaseClone\PSDatabaseClone.psd1" -ModuleVersion "$($localVersion.Major).$($localVersion.Minor).$($newBuildNumber)"
 }
 
 #region Publish
@@ -130,13 +130,13 @@ if ($LocalRepo) {
     # Dependencies must go first
     Write-PSFMessage -Level Important -Message "Creating Nuget Package for module: PSFramework"
     New-PSMDModuleNugetPackage -ModulePath (Get-Module -Name PSFramework).ModuleBase -PackagePath .
-    Write-PSFMessage -Level Important -Message "Creating Nuget Package for module: PStSQLtTestGenerator"
-    New-PSMDModuleNugetPackage -ModulePath "$($publishDir.FullName)\PStSQLtTestGenerator" -PackagePath .
+    Write-PSFMessage -Level Important -Message "Creating Nuget Package for module: PSDatabaseClone"
+    New-PSMDModuleNugetPackage -ModulePath "$($publishDir.FullName)\PSDatabaseClone" -PackagePath .
 }
 else {
     # Publish to Gallery
-    Write-PSFMessage -Level Important -Message "Publishing the PStSQLtTestGenerator module to $($Repository)"
-    Publish-Module -Path "$($publishDir.FullName)\PStSQLtTestGenerator" -NuGetApiKey $ApiKey -Force -Repository $Repository
+    Write-PSFMessage -Level Important -Message "Publishing the PSDatabaseClone module to $($Repository)"
+    Publish-Module -Path "$($publishDir.FullName)\PSDatabaseClone" -NuGetApiKey $ApiKey -Force -Repository $Repository
 }
 #endregion Updating the Module Version
 
