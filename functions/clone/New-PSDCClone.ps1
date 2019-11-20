2d47cd30b9143994008668c556c605b68362fddb
sqlstad <sanderstad@gmail.com>
2019-11-20 10:35:07 +0100

    Changed acl to work with different languages fixes #60

--- functions/clone/New-PSDCClone.ps1
+++ functions/clone/New-PSDCClone.ps1
@@ -471,7 +471,11 @@
                 try {
                     # Check if computer is local
                     if ($computer.IsLocalhost) {
-                        $accessRule = New-Object System.Security.AccessControl.FilesystemAccessrule("Everyone", "FullControl", "Allow")
+                        #$accessRule = New-Object System.Security.AccessControl.FilesystemAccessrule("Everyone", "FullControl", "Allow")
+
+                        $everyone = [System.Security.Principal.WellKnownSidType]::WorldSid
+                        $sid = New-Object System.Security.Principal.SecurityIdentifier($everyone, $Null)
+                        $accessRule = New-Object System.Security.AccessControl.FilesystemAccessrule($sid, "FullControl", "Allow")
 
                         foreach ($file in $(Get-ChildItem -Path $accessPath -Recurse)) {
                             $acl = Get-Acl $file.FullName
@@ -484,7 +488,11 @@
                         }
                     }
                     else {
-                        [string]$commandText = "`$accessRule = New-Object System.Security.AccessControl.FilesystemAccessrule(`"Everyone`", `"FullControl`", `"Allow`")
+                        [string]$commandText = "
+                            `$everyone = [System.Security.Principal.WellKnownSidType]::WorldSid
+                            `$sid = New-Object System.Security.Principal.SecurityIdentifier(`$everyone, $Null)
+                            `$accessRule = New-Object System.Security.AccessControl.FilesystemAccessrule(`$sid, `"FullControl`", `"Allow`")
+                            `$accessRule = New-Object System.Security.AccessControl.FilesystemAccessrule(`"Everyone`", `"FullControl`", `"Allow`")
                             foreach (`$file in `$(Get-ChildItem -Path `"$accessPath`" -Recurse)) {
                                 `$acl = Get-Acl `$file.Fullname
 
