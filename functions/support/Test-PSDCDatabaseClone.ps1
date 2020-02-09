79a83ce2756239d99a1e702b04c08a27dfe671a4
sqlstad <sanderstad@gmail.com>
2020-02-09 10:29:10 +0100

    Added windows version check

--- functions/support/Test-PSDCDatabaseClone.ps1
+++ functions/support/Test-PSDCDatabaseClone.ps1
@@ -11,6 +11,24 @@ function Test-PSDCDatabaseClone {
     .PARAMETER SetupStatus
         Setup status should be set.
 
+    .PARAMETER WindowsVersion
+        Tests if the windows version running is the correct one.
+
+        Windows version should be in
+            - 'Microsoft Windows 10 Pro',
+            - 'Microsoft Windows 10 Enterprise',
+            - 'Microsoft Windows 10 Education',
+            - 'Microsoft Windows Server 2008 R2 Standard',
+            - 'Microsoft Windows Server 2008 R2 Enterprise',
+            - 'Microsoft Windows Server 2008 R2 Datacenter'
+            - 'Microsoft Windows Server 2012 R2 Standard',
+            - 'Microsoft Windows Server 2012 R2 Enterprise',
+            - 'Microsoft Windows Server 2012 R2 Datacenter',
+            - 'Microsoft Windows Server 2016 Standard',
+            - 'Microsoft Windows Server 2016 Enterprise',
+            - 'Microsoft Windows Server 2016 Datacenter'
+            - 'Microsoft Windows Server 2019 Datacenter'
+
     .PARAMETER WhatIf
         If this switch is enabled, no actions are performed but informational messages will be displayed that explain what would happen if the command were to run.
 
@@ -34,7 +52,8 @@ function Test-PSDCDatabaseClone {
     #>
 
     param(
-        [switch]$SetupStatus
+        [switch]$SetupStatus,
+        [switch]$WindowsVersion
     )
 
     begin {
@@ -49,6 +68,41 @@ function Test-PSDCDatabaseClone {
             }
             else {
                 return $true
+            }
+        }
+
+        if ($WindowsVersion) {
+            $supportedVersions = @(
+                'Microsoft Windows 10 Pro',
+                'Microsoft Windows 10 Enterprise',
+                'Microsoft Windows 10 Education',
+                'Microsoft Windows Server 2008 R2 Standard',
+                'Microsoft Windows Server 2008 R2 Enterprise',
+                'Microsoft Windows Server 2008 R2 Datacenter'
+                'Microsoft Windows Server 2012 R2 Standard',
+                'Microsoft Windows Server 2012 R2 Enterprise',
+                'Microsoft Windows Server 2012 R2 Datacenter',
+                'Microsoft Windows Server 2016 Standard',
+                'Microsoft Windows Server 2016 Enterprise',
+                'Microsoft Windows Server 2016 Datacenter'
+                'Microsoft Windows Server 2019 Datacenter'
+            )
+
+            # Get the OS details
+            $osDetails = Get-CimInstance Win32_OperatingSystem | Select-Object Caption, Description, Name, OSType, Version
+
+            $windowsEdition = ($osDetails.Caption).Replace(" Evaluation", "").Trim()
+
+            # Check which version of windows we're dealing with
+            if ($windowsEdition -notin $supportedVersions ) {
+                if ($windowsEdition -like '*Windows 7*') {
+                    return $false
+                    #Stop-PSFFunction -Message "Module does not work on Windows 7" -Target $OSDetails -FunctionName 'Pre Import'
+                }
+                else {
+                    #Stop-PSFFunction -Message "Unsupported version of Windows." -Target $OSDetails -FunctionName 'Pre Import'
+                    return $false
+                }
             }
         }
 
