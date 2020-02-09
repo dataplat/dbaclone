9ee06a540c5abe633272a685330fef4049eb00d3
sqlstad <sanderstad@gmail.com>
2020-02-09 10:29:25 +0100

    Replaced windows version check with function

--- internal/scripts/preimport.ps1
+++ internal/scripts/preimport.ps1
@@ -1,33 +1,5 @@
-# Set the supported version of Windows
-$supportedVersions = @(
-    'Microsoft Windows 10 Pro',
-    'Microsoft Windows 10 Enterprise',
-    'Microsoft Windows 10 Education',
-    'Microsoft Windows Server 2008 R2 Standard',
-    'Microsoft Windows Server 2008 R2 Enterprise',
-    'Microsoft Windows Server 2008 R2 Datacenter'
-    'Microsoft Windows Server 2012 R2 Standard',
-    'Microsoft Windows Server 2012 R2 Enterprise',
-    'Microsoft Windows Server 2012 R2 Datacenter',
-    'Microsoft Windows Server 2016 Standard',
-    'Microsoft Windows Server 2016 Enterprise',
-    'Microsoft Windows Server 2016 Datacenter'
-    'Microsoft Windows Server 2019 Datacenter'
-)
-
-# Get the OS details
-$osDetails = Get-CimInstance Win32_OperatingSystem | Select-Object Caption, Description, Name, OSType, Version
-
-$windowsEdition = ($osDetails.Caption).Replace(" Evaluation", "").Trim()
-
-# Check which version of windows we're dealing with
-if ($windowsEdition -notin $supportedVersions ) {
-    if ($windowsEdition -like '*Windows 7*') {
-        Stop-PSFFunction -Message "Module does not work on Windows 7" -Target $OSDetails -FunctionName 'Pre Import'
-    }
-    else {
-        Stop-PSFFunction -Message "Unsupported version of Windows." -Target $OSDetails -FunctionName 'Pre Import'
-    }
+if (-not (Test-PSDCDatabaseClone -WindowsVersion)) {
+    Stop-PSFFunction -Message "Unsupported version of Windows" -FunctionName 'Pre Import'
 }
 
 if (-not (Test-Path -Path "$env:APPDATA\psdatabaseclone")) {
