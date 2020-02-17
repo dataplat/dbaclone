b4610d533b40d5540779bd90057b5145cc2298f0
sqlstad <sanderstad@gmail.com>
2020-02-17 16:00:51 +0100

    Replaced code with command

--- functions/clone/New-DcnClone.ps1
+++ functions/clone/New-DcnClone.ps1
@@ -485,51 +485,6 @@
                     }
                 }
 
-                # Set privileges for access path
-                try {
-                    # Check if computer is local
-                    if ($computer.IsLocalhost) {
-                        #$accessRule = New-Object System.Security.AccessControl.FilesystemAccessrule("Everyone", "FullControl", "Allow")
-
-                        $everyone = [System.Security.Principal.WellKnownSidType]::WorldSid
-                        $sid = New-Object System.Security.Principal.SecurityIdentifier($everyone, $Null)
-                        $accessRule = New-Object System.Security.AccessControl.FilesystemAccessrule($sid, "FullControl", "Allow")
-
-                        foreach ($file in $(Get-ChildItem -Path $accessPath -Recurse)) {
-                            $acl = Get-Acl $file.FullName
-
-                            # Add this access rule to the ACL
-                            $acl.SetAccessRule($accessRule)
-
-                            # Write the changes to the object
-                            Set-Acl -Path $file.Fullname -AclObject $acl
-                        }
-                    }
-                    else {
-                        [string]$commandText = "
-                            `$everyone = [System.Security.Principal.WellKnownSidType]::WorldSid
-                            `$sid = New-Object System.Security.Principal.SecurityIdentifier(`$everyone, $Null)
-                            `$accessRule = New-Object System.Security.AccessControl.FilesystemAccessrule(`$sid, `"FullControl`", `"Allow`")
-                            `$accessRule = New-Object System.Security.AccessControl.FilesystemAccessrule(`"Everyone`", `"FullControl`", `"Allow`")
-                            foreach (`$file in `$(Get-ChildItem -Path `"$accessPath`" -Recurse)) {
-                                `$acl = Get-Acl `$file.Fullname
-
-                                # Add this access rule to the ACL
-                                `$acl.SetAccessRule(`$accessRule)
-
-                                # Write the changes to the object
-                                Set-Acl -Path `$file.Fullname -AclObject `$acl
-                            }"
-
-                        $command = [scriptblock]::Create($commandText)
-
-                        $null = Invoke-PSFCommand -ComputerName $computer -ScriptBlock $command -ArgumentList $accessPath -Credential $DestinationCredential
-                    }
-                }
-                catch {
-                    Stop-PSFFunction -Message "Couldn't create access path directory" -ErrorRecord $_ -Target $accessPath -Continue
-                }
-
                 if (-not $SkipDatabaseMount) {
                     # Get all the files of the database
                     if ($computer.IsLocalhost) {
@@ -692,6 +647,25 @@
                     elseif ($informationStore -eq 'File') {
                         $hostID = ($hosts | Where-Object { $_.Hostname -eq $hostname } | Select-Object HostID -Unique).HostID
                     }
+                }
+
+                # Set privileges for access path
+                try {
+                    # Check if computer is local
+                    if ($computer.IsLocalhost) {
+                        $accessPath
+                        Set-DcnPermission -Path $accessPath
+                    }
+                    else {
+                        [string]$commandText = "Set-DcnPermission -Path '$($accessPath)'"
+
+                        $command = [scriptblock]::Create($commandText)
+
+                        $null = Invoke-PSFCommand -ComputerName $computer -ScriptBlock $command -Credential $DestinationCredential
+                    }
+                }
+                catch {
+                    Stop-PSFFunction -Message "Couldn't create access path directory" -ErrorRecord $_ -Target $accessPath -Continue
                 }
 
                 # Setup the clone location
