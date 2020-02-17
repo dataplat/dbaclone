cf0c9587c6932a05cb4bd36a43b6560be9008f8d
sqlstad <sanderstad@gmail.com>
2020-02-17 12:18:18 +0100

    Added TEPP for several commands

--- internal/tepp/assignment.ps1
+++ internal/tepp/assignment.ps1
@@ -2,3 +2,20 @@
 # Example:
 Register-PSFTeppArgumentCompleter -Command Get-Alcohol -Parameter Type -Name dbaclone.alcohol
 #>
+
+# Image commands
+Register-PSFTeppArgumentCompleter -Command Get-DcnImage -Parameter ImageID -Name 'dbaclone.images.id'
+Register-PSFTeppArgumentCompleter -Command Get-DcnImage -Parameter ImageName -Name 'dbaclone.images.name'
+Register-PSFTeppArgumentCompleter -Command Remove-DcnImage -Parameter Database -Name 'dbaclone.images.database'
+Register-PSFTeppArgumentCompleter -Command Remove-DcnImage -Parameter ImageID -Name 'dbaclone.images.id'
+Register-PSFTeppArgumentCompleter -Command Remove-DcnImage -Parameter ImageName -Name 'dbaclone.images.name'
+Register-PSFTeppArgumentCompleter -Command Remove-DcnImage -Parameter ImageLocation -Name 'dbaclone.images.location'
+
+# Clone commands
+Register-PSFTeppArgumentCompleter -Command Get-DcnClone -Parameter Database -Name 'dbaclone.clones.databasename'
+Register-PSFTeppArgumentCompleter -Command Get-DcnClone -Parameter HostName -Name 'dbaclone.clones.hostname'
+Register-PSFTeppArgumentCompleter -Command Get-DcnClone -Parameter ImageID -Name 'dbaclone.clones.imageid'
+Register-PSFTeppArgumentCompleter -Command Get-DcnClone -Parameter ImageName -Name 'dbaclone.clones.imagename'
+Register-PSFTeppArgumentCompleter -Command Remove-DcnClone -Parameter Database -Name 'dbaclone.clones.databasename'
+Register-PSFTeppArgumentCompleter -Command Remove-DcnClone -Parameter HostName -Name 'dbaclone.clones.hostname'
+
