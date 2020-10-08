024c7000e97c1470582f0007390d8436dbc0b80d
sqlstad <sanderstad@gmail.com>
2020-10-08 12:52:14 +0200

    Renamed branch to main

--- build/appveyor-build.ps1
+++ build/appveyor-build.ps1
@@ -112,7 +112,7 @@ Write-PSFMessage -Level Important -Message "Branch: $($env:APPVEYOR_REPO_BRANCH)
 #if ($env:APPVEYOR_REPO_BRANCH -eq 'master') {
 if ($SkipPublish) { return }
 
-if ($env:APPVEYOR_REPO_BRANCH -eq 'master') {
+if ($env:APPVEYOR_REPO_BRANCH -eq 'main') {
     if ($AutoVersion) {
         Write-PSFMessage -Level Important -Message "Updating module version numbers."
         try { [version]$remoteVersion = (Find-Module 'dbaclone' -Repository $Repository -ErrorAction Stop).Version }
