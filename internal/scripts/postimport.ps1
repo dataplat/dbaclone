aa331e395af3efc57d1267509a889c10046f6f69
sqlstad <sanderstad@gmail.com>
2020-10-08 12:22:46 +0200

    Moved code to fix import problem

--- internal/scripts/postimport.ps1
+++ internal/scripts/postimport.ps1
@@ -1,26 +1,5 @@
 ﻿# Add all things you want to run after importing the main code
 
-# Check the information mode
-if ([bool](Get-PSFConfigValue -FullName dbaclone.informationstore.mode) -eq 'File') {
-	# Get the json file
-	$jsonFolder = Get-PSFConfigValue -FullName dbaclone.informationstore.path
-	$jsonCred = Get-PSFConfigValue -FullName dbaclone.informationstore.credential -Fallback $null
-
-	# Create a PS Drive
-	if (-not [bool](Get-PSDrive -Name DCNJSONFolder -Scope Global -ErrorAction SilentlyContinue)) {
-		try {
-			$null = New-PSDrive -Name DCNJSONFolder -Root $jsonFolder -Credential $jsonCred -PSProvider FileSystem -Scope Global
-
-			while ((Get-PSDrive | Select-Object -ExpandProperty Name) -notcontains 'DCNJSONFolder') {
-				Start-Sleep -Milliseconds 500
-			}
-		}
-		catch {
-			Stop-PSFFunction -Message "Couldn't create PS Drive" -Target $jsonFolder -ErrorRecord $_
-		}
-	}
-}
-
 # Load Configurations
 foreach ($file in (Get-ChildItem "$($script:ModuleRoot)\internal\configurations\*.ps1" -ErrorAction Ignore)) {
 	. Import-ModuleFile -Path $file.FullName
@@ -41,6 +20,27 @@ foreach ($file in (Get-ChildItem "$($script:ModuleRoot)\internal\tepp\*.tepp.ps1
 
 # Load License
 . Import-ModuleFile -Path "$($script:ModuleRoot)\internal\scripts\license.ps1"
+
+# Check the information mode
+if ([bool](Get-PSFConfigValue -FullName dbaclone.informationstore.mode) -eq 'File') {
+	# Get the json file
+	$jsonFolder = Get-PSFConfigValue -FullName dbaclone.informationstore.path
+	$jsonCred = Get-PSFConfigValue -FullName dbaclone.informationstore.credential -Fallback $null
+
+	# Create a PS Drive
+	if (-not [bool](Get-PSDrive -Name DCNJSONFolder -Scope Global -ErrorAction SilentlyContinue)) {
+		try {
+			$null = New-PSDrive -Name DCNJSONFolder -Root $jsonFolder -Credential $jsonCred -PSProvider FileSystem -Scope Global
+
+			while ((Get-PSDrive | Select-Object -ExpandProperty Name) -notcontains 'DCNJSONFolder') {
+				Start-Sleep -Milliseconds 500
+			}
+		}
+		catch {
+			Stop-PSFFunction -Message "Couldn't create PS Drive" -Target $jsonFolder -ErrorRecord $_
+		}
+	}
+}
 
 # Check if window is in elevated mode
 if ( -not (Test-PSFPowerShell -Elevated) ) {
