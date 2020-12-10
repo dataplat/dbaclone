3b4cc8677c55b6331195970f4c1404af67419c4e
sqlstad <sanderstad@gmail.com>
2020-12-10 12:35:08 +0100

    Removed white space

--- dbaclone.psm1
+++ dbaclone.psm1
@@ -18,25 +18,24 @@ $importIndividualFiles = Get-PSFConfigValue -FullName dbaclone.Import.Individual
 if ($dbaclone_importIndividualFiles) { $importIndividualFiles = $true }
 if (Test-Path (Resolve-PSFPath -Path "$($script:ModuleRoot)\..\.git" -SingleItem -NewChild)) { $importIndividualFiles = $true }
 if ("<was not compiled>" -eq '<was not compiled>') { $importIndividualFiles = $true }
-	
-function Import-ModuleFile
-{
+
+function Import-ModuleFile {
 	<#
 		.SYNOPSIS
 			Loads files into the module on module import.
-		
+
 		.DESCRIPTION
 			This helper function is used during module initialization.
 			It should always be dotsourced itself, in order to proper function.
-			
+
 			This provides a central location to react to files being imported, if later desired
-		
+
 		.PARAMETER Path
 			The path to the file to load
-		
+
 		.EXAMPLE
 			PS C:\> . Import-ModuleFile -File $function.FullName
-	
+
 			Imports the file stored in $function according to import policy
 	#>
 	[CmdletBinding()]
@@ -44,33 +43,30 @@ function Import-ModuleFile
 		[string]
 		$Path
 	)
-	
+
 	$resolvedPath = $ExecutionContext.SessionState.Path.GetResolvedPSPathFromPSPath($Path).ProviderPath
 	if ($doDotSource) { . $resolvedPath }
 	else { $ExecutionContext.InvokeCommand.InvokeScript($false, ([scriptblock]::Create([io.file]::ReadAllText($resolvedPath))), $null, $null) }
 }
 
 #region Load individual files
-if ($importIndividualFiles)
-{
+if ($importIndividualFiles) {
 	# Execute Preimport actions
 	. Import-ModuleFile -Path "$ModuleRoot\internal\scripts\preimport.ps1"
-	
+
 	# Import all internal functions
-	foreach ($function in (Get-ChildItem "$ModuleRoot\internal\functions" -Filter "*.ps1" -Recurse -ErrorAction Ignore))
-	{
+	foreach ($function in (Get-ChildItem "$ModuleRoot\internal\functions" -Filter "*.ps1" -Recurse -ErrorAction Ignore)) {
 		. Import-ModuleFile -Path $function.FullName
 	}
-	
+
 	# Import all public functions
-	foreach ($function in (Get-ChildItem "$ModuleRoot\functions" -Filter "*.ps1" -Recurse -ErrorAction Ignore))
-	{
+	foreach ($function in (Get-ChildItem "$ModuleRoot\functions" -Filter "*.ps1" -Recurse -ErrorAction Ignore)) {
 		. Import-ModuleFile -Path $function.FullName
 	}
-	
+
 	# Execute Postimport actions
 	. Import-ModuleFile -Path "$ModuleRoot\internal\scripts\postimport.ps1"
-	
+
 	# End it here, do not load compiled code below
 	return
 }
