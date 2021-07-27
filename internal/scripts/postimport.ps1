db478b3dd928bb2f4f31389a0b019750b539d5a9
Kyle Burns <kyle.burns@koder.ly>
2021-07-27 12:01:33 +0100

    Changed the logic to make it easier to read and more robust.

--- internal/scripts/postimport.ps1
+++ internal/scripts/postimport.ps1
@@ -28,7 +28,7 @@ if ((Get-PSFConfigValue -FullName dbaclone.informationstore.mode).ToLower() -eq 
     $jsonCred = Get-PSFConfigValue -FullName dbaclone.informationstore.credential -Fallback $null
 
     # Create a PS Drive
-	if (-not [bool](Get-PSDrive -Name DCNJSONFolder -Scope Global -ErrorAction SilentlyContinue)) {
+    if (Get-PSDrive -Name DCNJSONFolder -Scope Global -ErrorAction SilentlyContinue -eq $null) {
         try {
             $null = New-PSDrive -Name DCNJSONFolder -Root $jsonFolder -Credential $jsonCred -PSProvider FileSystem -Scope Global
 
