ac7c1236d285ed57c6db4922e06584149cae8dd5
Kyle Burns <kyle.burns@koder.ly>
2021-08-18 10:03:23 +0100

    Changing SQL instance to use the destination instead of source.

--- functions/image/New-DcnImage.ps1
+++ functions/image/New-DcnImage.ps1
@@ -309,8 +309,8 @@
         {
             # Check the image local path
             if ($PSCmdlet.ShouldProcess("Verifying image local path")) {
-                if ((Test-DbaPath -Path $ImageLocalPath -SqlInstance $SourceSqlInstance -SqlCredential $DestinationCredential) -ne $true) {
-                    Stop-PSFFunction -Message "Image local path $ImageLocalPath is not valid directory or can't be reached." -Target $SourceSqlInstance
+                if ((Test-DbaPath -Path $ImageLocalPath -SqlInstance $DestinationSqlCredential -SqlCredential $DestinationCredential) -ne $true) {
+                    Stop-PSFFunction -Message "Image local path $ImageLocalPath is not valid directory or can't be reached." -Target $DestinationSqlCredential
                     return
                 }
             }
