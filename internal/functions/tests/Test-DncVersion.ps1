7e6fe6a71ad5e11083d87ded704d2e053c86c160
Sander Stad <sanderstad@gmail.com>
2021-09-09 09:47:41 +0200

    Added help Added outputtype Refers to #139

--- internal/functions/tests/Test-DncVersion.ps1
+++ internal/functions/tests/Test-DncVersion.ps1
@@ -6,6 +6,17 @@ function Test-DcnVersion {
     .DESCRIPTION
         Test if the current version of the module is up-to-date with the PSGallery
 
+    .PARAMETER EnableException
+        By default, when something goes wrong we try to catch it, interpret it and give you a friendly warning message.
+        This avoids overwhelming you with "sea of red" exceptions, but is inconvenient because it basically disables advanced scripting.
+        Using this switch turns this "nice by default" feature off and enables you to catch exceptions with your own try/catch.
+
+    .PARAMETER WhatIf
+        If this switch is enabled, no actions are performed but informational messages will be displayed that explain what would happen if the command were to run.
+
+    .PARAMETER Confirm
+        If this switch is enabled, you will be prompted for confirmation before executing any operations that change state.
+
     .NOTES
         Author: Sander Stad (@sqlstad, sqlstad.nl)
 
@@ -22,6 +33,8 @@ function Test-DcnVersion {
         Test the configuration of the module retrieving the set configurations
 
     #>
+
+    [OutputType("System.Boolean")]
 
     [CmdLetBinding()]
 
